1173bc9473495df73a74e409681c587b466f3e8b
chennaarc <gottamc@yahoo.com>
2018-05-01 21:24:29 -0400

    Update Sanitization.java

--- demo/src/main/java/com/util/Sanitization.java
+++ demo/src/main/java/com/util/Sanitization.java
@@ -7,4 +7,10 @@ public class Sanitization {
 	public static String processSantizeData(String requestStr) {
 		return HtmlUtils.htmlEscape(requestStr);
 	}
+	
+	public static void processSantizeData(Employee emp) {
+		emp.setFirstName(HtmlUtils.htmlEscape(emp.getFirstName()));
+		emp.setLastName(HtmlUtils.htmlEscape(emp.getLastName()));
+		emp.setEmpId(HtmlUtils.htmlEscape(emp.getEmpId()));
+	}	
 }
